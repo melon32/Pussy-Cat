ff0906c214f775614cf136336cba962389b7c276
melon32 <andersonsandris890@gmail.com>
2023-03-19 21:05:34 +0200

    done

--- main.py
+++ main.py
@@ -33,14 +33,30 @@ def collisions(player, obstacles):
     return True
 
 
+def player_animation():
+    global player_surface, player_index
+
+    if player_rect.bottom < 305:
+        player_surface = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk): player_index = 0
+        player_surface = player_walk[int(player_index)]
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Cat Runner')
+pygame.display.set_caption('Pussy Cat')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 40)
 game_active = True
 start_time = 0
 score = 0
+jump_sound = pygame.mixer.Sound('graphics/audio/jump.mp3')
+jump_sound.set_volume(0.5)
+bg_music = pygame.mixer.Sound('graphics/audio/bgmusic1.mp3')
+bg_music.set_volume(0.5)
+bg_music.play(loops= -1)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -49,20 +65,43 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 # score_rect = score_surf.get_rect(center = (400,50))
 
 #obstacles
-varna_surf = pygame.image.load('graphics/varna/varna1.png').convert_alpha()
-balodis_surf = pygame.image.load('graphics/balodis/balodis1.png').convert_alpha()
+
+
+varna_frame_1 = pygame.image.load('graphics/varna/varna1.png').convert_alpha()
+varna_frame_2 = pygame.image.load('graphics/varna/varna2.png').convert_alpha()
+varna_frames = [varna_frame_1, varna_frame_2]
+varna_frame_index = 0
+varna_surf = varna_frames[varna_frame_index]
+
+balodis_frame_1 = pygame.image.load('graphics/balodis/balodis1.png').convert_alpha()
+balodis_frame_2 = pygame.image.load('graphics/balodis/balodis2.png').convert_alpha()
+balodis_frames = [balodis_frame_1, balodis_frame_2]
+balodis_frame_index = 0
+balodis_surf = balodis_frames[balodis_frame_index]
+
 
 obstacle_rect_list = []
 
-player_surface = pygame.image.load('graphics/player/player1.png').convert_alpha()
-player_rect = player_surface.get_rect(midbottom = (80,300))
+
+player_walk_1 = pygame.image.load('graphics/player/walk1.png').convert_alpha()
+player_walk_2 = pygame.image.load('graphics/player/walk2.png').convert_alpha()
+player_walk_3 = pygame.image.load('graphics/player/walk3.png').convert_alpha()
+player_walk_4 = pygame.image.load('graphics/player/walk4.png').convert_alpha()
+
+player_walk = [player_walk_1, player_walk_2, player_walk_3, player_walk_4]
+player_index = 0
+
+player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+
+player_surface = player_walk[player_index]
+player_rect = player_surface.get_rect(midbottom = (130,305))
 player_grav = 0
 
 player_stand = pygame.image.load('graphics/player/angry.png').convert_alpha()
 player_stand = pygame.transform.scale(player_stand, (150, 150))
 player_stand_rect = player_stand.get_rect(center = (400, 100))
 
-game_name = test_font.render('Cat Runner', False, (255,255,255))
+game_name = test_font.render('Pussy Cat', False, (255,255,255))
 game_name_rect = game_name.get_rect(center = (400, 200))
 
 game_message = test_font.render('Press space to run', False, (115,0,0))
@@ -71,6 +110,13 @@ game_message_rect = game_message.get_rect(center = (400, 350))
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1400)
 
+varna_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(varna_animation_timer, 500)
+
+balodis_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(balodis_animation_timer, 500)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -78,24 +124,35 @@ while True:
             exit()
         
         if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player_rect.collidepoint(event.pos)and player_rect.bottom >= 300:
-                    player_grav = -21
-
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 305:
                     player_grav = -21
+                    jump_sound.play()
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 
                 start_time = int(pygame.time.get_ticks() / 1000)
 
-        if event.type == obstacle_timer and game_active:
-            if randint(0,2):
-                obstacle_rect_list.append(varna_surf.get_rect(bottomright = (randint(900, 1100), 300)))
-            else: 
-                obstacle_rect_list.append(balodis_surf.get_rect(bottomright = (randint(900, 1100), 240)))
+
+        if game_active:
+            if event.type == obstacle_timer:
+                if randint(0,2):
+                    obstacle_rect_list.append(varna_surf.get_rect(bottomright = (randint(900, 1100), 300)))
+                else: 
+                    obstacle_rect_list.append(balodis_surf.get_rect(bottomright = (randint(900, 1100), 240)))
+            
+            if event.type == varna_animation_timer:
+                if varna_frame_index == 0: varna_frame_index = 1
+                else: varna_frame_index = 0
+                varna_surf = varna_frames[varna_frame_index]
+
+            if event.type == balodis_animation_timer:
+                if balodis_frame_index == 0: balodis_frame_index = 1
+                else: balodis_frame_index = 0
+                balodis_surf = balodis_frames[balodis_frame_index]
+
+        
 
  
     if game_active:
@@ -111,7 +168,8 @@ while True:
         #player
         player_grav += 1
         player_rect.y += player_grav
-        if player_rect.bottom >= 300: player_rect.bottom = 300
+        if player_rect.bottom >= 305: player_rect.bottom = 305
+        player_animation()
         screen.blit(player_surface, player_rect)
 
         #obstacle movement
@@ -124,7 +182,7 @@ while True:
         screen.fill((50,50,50))
         screen.blit(player_stand, player_stand_rect)
         obstacle_rect_list.clear()
-        player_rect.midbottom = (80, 300)
+        player_rect.midbottom = (130, 300)
         player_grav = 0
 
         score_message = test_font.render(f'SCORE: {score}', False, (255,255,255))
